41a4ba4f0030629c6c49deddbb0411f832864765
yu405405 <66393193+you405405@users.noreply.github.com>
2022-10-27 08:31:22 +0900

    文字列の長さ, 文字列の置換, 文字列を取得するメソッドについて

--- chap05/mb_substr.php
+++ chap05/mb_substr.php
@@ -1,10 +1,9 @@
 <?php
 mb_internal_encoding('UTF-8');
 $str = 'WINGSプロジェクト';
-print mb_substr($str, 5, 2);
-print '<br />';
-print mb_substr($str, 5);
-print '<br />';
-print mb_substr($str, 5, -4);
-print '<br />';
+
+print mb_substr($str, 5, 2) . '<br>';
+print mb_substr($str, 0, 5) . '<br>';
+print mb_substr($str, 5) . '<br>';
+print mb_substr($str, 5, -4) . '<br>';
 print mb_substr($str, -6, 2);
