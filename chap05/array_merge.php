8bc60317d53c5dd0f1d2efc08ed629976a07a531
yu405405 <66393193+you405405@users.noreply.github.com>
2022-10-30 16:35:29 +0900

    配列に関する組み込み関数について

--- chap05/array_merge.php
+++ chap05/array_merge.php
@@ -2,11 +2,9 @@
 <?php
 $ary1 = [1, 3, 5];
 $ary2 = [2, 3, 6];
-$result = array_merge($ary1, $ary2);
-print_r($result);
+print_r(array_merge($ary1, $ary2));
 
 $assoc1 = ['Apple' => 'Red', 'Orange' => 'Yellow', 'Melon' => 'Green'];
 $assoc2 = ['Grape' => 'Purple', 'Apple' => 'Green', 'Strawberry' => 'Red'];
-$result = array_merge($assoc1, $assoc2);
-// $result = array_merge_recursive($assoc1, $assoc2);
-print_r($result);
+print_r(array_merge($assoc1, $assoc2));
+print_r(array_merge_recursive($assoc1, $assoc2));
