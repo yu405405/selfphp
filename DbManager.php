718e38e8c4c3b7a9fedf3b757a3253ed29486aba
yu405405 <66393193+you405405@users.noreply.github.com>
2022-12-11 12:24:14 +0900

    2ファイルの更新

--- DbManager.php
+++ DbManager.php
@@ -1,11 +1,10 @@
 <?php
 function getDb() {
   $dsn = 'mysql:dbname=selfphp; host=127.0.0.1; charset=utf8';
-  $usr = 'selfusr';
-  $passwd = 'selfpass';
+  $usr = 'root';
+  $passwd = 'root';
 
-    $db = new PDO($dsn, $usr, $passwd);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //$db = new PDO($dsn, $usr, $passwd, [PDO::ATTR_PERSISTENT => true);
+  $db = new PDO($dsn, $usr, $passwd);
+  
   return $db;
 }
--- chap09/connect.php
+++ chap09/connect.php
@@ -1,13 +1,13 @@
 <?php
 $dsn = 'mysql:dbname=selfphp; host=127.0.0.1; charset=utf8';
-$usr = 'selfusr';
-$passwd = 'selfpass';
+$usr = 'root';
+$passwd = 'root';
 
 try {
   $db = new PDO($dsn, $usr, $passwd);
   print '接続に成功しました。';
-} catch (PDOException $e) {
-  print "接続エラー：{$e->getMessage()}";
+} catch(PDOException $e) {
+  print "接続エラー：{$e -> getMessage()}";
 } finally {
   $db = null;
 }
